91ccefab7a70076f1cbbcbdf68b628d1beab647b
oldboy.dev <pm@oldboy.dev>
2020-06-09 18:31:45 -0300

    created routes to cities with big and small name sizes

--- modulo_02/trabalho/src/controllers/StatesController.js
+++ modulo_02/trabalho/src/controllers/StatesController.js
@@ -29,21 +29,37 @@ class StatesController{
 
     async showLargestStatesByCitiesNumber(req, res){
         const { top } = req.params;        
-        const citiesNumberByStates = [];
-        
-        const statesFiles = await fs.readdir(this.folderPath);
 
-        for(let file of statesFiles){
-            const uf = file.split(".")[0];
-            const countCities = await this.getCitiesNumByState(uf.toUpperCase());
-
-            citiesNumberByStates.push({ state: uf, numberOfCities: countCities });
+        if(!Number.isInteger(Number.parseInt(top))){
+            return res.status(400).json({ msg: "top parameter must be a integer!"});
         }
+        
+        try{
+            const topNLargestStates = await this.getStatesByCitiesNumber(top, "largest");
 
-        const largestStates = citiesNumberByStates.sort( (a, b) => a.numberOfCities < b.numberOfCities ? 1 : -1);
-        const topNLargestStates = largestStates.filter((state, index) => index < top);
+            return res.json(topNLargestStates);
+        } catch(error){
+            console.error(error)
+            return res.status(400).json({ msg: error});
+        }
+    }
 
-        console.log(topNLargestStates);
+    async showSmallestStatesByCitiesNumber(req, res){
+        const { top } = req.params;        
+
+        if(!Number.isInteger(Number.parseInt(top))){
+            return res.status(400).json({ msg: "top parameter must be a integer!"});
+        }
+        
+        try{
+            const topNSmallestStates = await this.getStatesByCitiesNumber(top, "small");
+            const orderedTopNSmallestStates = topNSmallestStates.sort( (a, b) => a.numberOfCities < b.numberOfCities ? 1 : -1);
+
+            return res.json(orderedTopNSmallestStates);
+        } catch(error){
+            console.error(error)
+            return res.status(400).json({ msg: error});
+        }
     }
 
     async getCitiesNumByState(state){
@@ -51,6 +67,83 @@ class StatesController{
         const cities = await this.utils.readFile(fileName, this.folderPath);
 
         return JSON.parse(cities).length;
+    }
+
+    async getStatesByCitiesNumber(top, order){
+        const citiesNumberByStates = [];
+        const statesFiles = await fs.readdir(this.folderPath);
+        
+        for(let file of statesFiles){
+            const uf = file.split(".")[0];
+            const countCities = await this.getCitiesNumByState(uf.toUpperCase());
+
+            citiesNumberByStates.push({ state: uf, numberOfCities: countCities, label: uf + "-" + countCities });
+        }
+
+        let statesCitiesNumber;
+
+        if(order == "largest"){
+            statesCitiesNumber = citiesNumberByStates.sort( (a, b) => a.numberOfCities < b.numberOfCities ? 1 : -1);
+        }else{
+            statesCitiesNumber = citiesNumberByStates.sort( (a, b) => b.numberOfCities < a.numberOfCities ? 1 : -1);
+        }
+
+        const topNStates = statesCitiesNumber.filter((state, index) => index < top);
+
+        return topNStates;
+    }
+
+    async showCompareCitiesNameSize(req, res){
+        const { type } = req.params;
+
+        if(type != "big" && type != "small"){
+            return res.status(400).json({ msg: "type parameter must be big or small"});
+        }
+
+        try{
+            const citiesBigNames = await this.getCitiesSortedByNameSize(type);
+
+            return res.json(citiesBigNames);
+        }catch(error){
+            console.error(error)
+            return res.status(400).json({ msg: error});
+        }
+    }
+
+    async getCitiesSortedByNameSize(type){
+        const citiesBigNames = [];
+        const statesFiles = await fs.readdir(this.folderPath);
+        
+        for(let file of statesFiles){
+            const uf = file.split(".")[0];
+            const fileName = uf.toUpperCase() + ".json";
+
+            const jsonCities = await this.utils.readFile(fileName, this.folderPath);
+            const cities = JSON.parse(jsonCities);
+            
+            let citiesSorted = [];
+
+            if(type == "big"){
+                citiesSorted = cities.sort(
+                    (a, b) => a.Nome.length < b.Nome.length ? 1 : -1
+                );
+            }else {
+                citiesSorted = cities.sort(
+                    (a, b) => a.Nome.length > b.Nome.length ? 1 : -1
+                );
+            }
+            
+            const cityWithBiggestName = citiesSorted[0];
+            citiesBigNames.push({ 
+                state: uf, 
+                city: cityWithBiggestName.Nome, 
+                label: cityWithBiggestName.Nome + "-" + uf
+            });
+        }
+
+        console.log(citiesBigNames);
+
+        return citiesBigNames;
     }
 }
 
--- modulo_02/trabalho/src/routes.js
+++ modulo_02/trabalho/src/routes.js
@@ -7,5 +7,7 @@ const statesController = new StatesController("src/files/estados");
 
 routes.get("/states/:uf", statesController.showCountCities.bind(statesController));
 routes.get("/states/largest/:top", statesController.showLargestStatesByCitiesNumber.bind(statesController));
+routes.get("/states/smallest/:top", statesController.showSmallestStatesByCitiesNumber.bind(statesController));
+routes.get("/states/name/:type", statesController.showCompareCitiesNameSize.bind(statesController));
 
 export default routes;
