bbf895a0a73974754cc4485b860fce536ac73b4b
oldboy.dev <pm@oldboy.dev>
2020-06-09 16:13:19 -0300

    Largest States by city number
    --criado rota /states/largest/:top a qual retorna os top estados com mais cidades

--- modulo_02/trabalho/src/controllers/StatesController.js
+++ modulo_02/trabalho/src/controllers/StatesController.js
@@ -1,4 +1,5 @@
 import Utils from "./../Utils.js";
+import { promises as fs } from "fs";
 
 class StatesController{
     folderPath = null;
@@ -17,21 +18,39 @@ class StatesController{
         }
 
         try{
-            const countCities = await getCitiesNumByState(uf.toUpperCase());
+            const countCities = await this.getCitiesNumByState(uf.toUpperCase());
 
             return res.json({ state: uf, numberOfCities: countCities});
         } catch(error){
             console.error(error)
-            return res.status(400).json({ msg: "invalid uf parameter! Can't find the state with uf: " + uf});
+            return res.status(400).json({ msg: "invalid uf parameter! Can't find the state with uf: " + uf.toUpperCase()});
         }        
     }
 
+    async showLargestStatesByCitiesNumber(req, res){
+        const { top } = req.params;        
+        const citiesNumberByStates = [];
+        
+        const statesFiles = await fs.readdir(this.folderPath);
+
+        for(let file of statesFiles){
+            const uf = file.split(".")[0];
+            const countCities = await this.getCitiesNumByState(uf.toUpperCase());
+
+            citiesNumberByStates.push({ state: uf, numberOfCities: countCities });
+        }
+
+        const largestStates = citiesNumberByStates.sort( (a, b) => a.numberOfCities < b.numberOfCities ? 1 : -1);
+        const topNLargestStates = largestStates.filter((state, index) => index < top);
+
+        console.log(topNLargestStates);
+    }
+
     async getCitiesNumByState(state){
-        console.log(state);
         const fileName = state + ".json";        
         const cities = await this.utils.readFile(fileName, this.folderPath);
 
-        return cities.length;
+        return JSON.parse(cities).length;
     }
 }
 
--- modulo_02/trabalho/src/routes.js
+++ modulo_02/trabalho/src/routes.js
@@ -5,6 +5,7 @@ import StatesController from "./controllers/StatesController.js";
 const routes = express.Router();
 const statesController = new StatesController("src/files/estados");
 
-routes.get("/states/:uf", (req, res) => statesController.showCountCities(req, res));
+routes.get("/states/:uf", statesController.showCountCities.bind(statesController));
+routes.get("/states/largest/:top", statesController.showLargestStatesByCitiesNumber.bind(statesController));
 
 export default routes;
